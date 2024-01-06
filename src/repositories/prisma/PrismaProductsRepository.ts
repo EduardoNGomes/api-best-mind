109dfea555fd514ac132f3b40abe196780a02c57
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-06 19:06:13 -0300

    feat: add Prisma Product Repository

---
+++ src/repositories/prisma/PrismaProductsRepository.ts
@@ -0,0 +1,34 @@
+import { PrismaClient, Products } from '@prisma/client'
+import {
+  ProductRepository,
+  ProductToCreateProps,
+  ProductToUpdateProps,
+} from '../ProductRepository'
+
+export class PrismaProductsRepository implements ProductRepository {
+  constructor(private prisma: PrismaClient) {}
+
+  async create(data: ProductToCreateProps): Promise<void> {
+    await this.prisma.products.create({ data })
+  }
+
+  async update(data: ProductToUpdateProps): Promise<void> {
+    await this.prisma.products.update({ data, where: { id: data.id } })
+  }
+
+  async findAll(): Promise<[] | Products[]> {
+    const allProducts = await this.prisma.products.findMany()
+
+    return allProducts
+  }
+
+  async findById(id: string): Promise<Products | null> {
+    const product = await this.prisma.products.findUnique({ where: { id } })
+
+    return product
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.prisma.products.delete({ where: { id } })
+  }
+}
