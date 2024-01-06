4b710eab1e95e1934da70fd45f1243dda32e9e66
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-06 18:48:37 -0300

    feat: add and run eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
-
 
 app.get('/', (req, reply) => {
   console.log('hello world')
   return reply.status(200).send()
 })
 
-app.listen({port:8080}).then(() => {console.log('server is running');})
+app.listen({ port: 8080 }).then(() => {
+  console.log('server is running')
+})
