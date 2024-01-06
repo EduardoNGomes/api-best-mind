6544fef7264e0a25029912cc5c3639101a91db36
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-06 15:00:24 -0300

    Start development

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+
+app.get('/', (req, reply) => {
+  console.log('hello world')
+  return reply.status(200).send()
+})
+
+app.listen({port:8080}).then(() => {console.log('server is running');})
