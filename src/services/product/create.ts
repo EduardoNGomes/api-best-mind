e067aced97eee1b412061ffbe60f779c38beba3c
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-07 12:13:17 -0300

    feat: add create service

---
+++ src/services/product/create.ts
@@ -0,0 +1,29 @@
+import { Either, left, right } from '@/erros/either'
+import { ProductRepository } from '@/repositories/ProductRepository'
+import { ResourceAlreadyExistError } from '../erros/ResourceAlreadyExistError'
+
+type CreateProductRequest = {
+  name: string
+  description: string
+  price: string
+}
+
+type CreateProductResponse = Either<ResourceAlreadyExistError, object>
+
+export class CreateProductService {
+  constructor(private productRepository: ProductRepository) {}
+
+  async execute({
+    name,
+    description,
+    price,
+  }: CreateProductRequest): Promise<CreateProductResponse> {
+    const productExist = await this.productRepository.findByName(name)
+
+    if (productExist) return left(new ResourceAlreadyExistError())
+
+    await this.productRepository.create({ name, description, price })
+
+    return right({})
+  }
+}
