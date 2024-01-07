e1dd3638b7e34ed73107fa71279ff331cf5b9547
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-07 20:26:40 -0300

    feat: add delete product spec

---
+++ src/services/product/Delete.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryProductsRepository } from '@/repositories/test/InMemoryProductsRepository'
+import { DeleteProductService } from './Delete'
+
+let sut: DeleteProductService
+let inMemoryProductsRepository: InMemoryProductsRepository
+
+const id = 'test-id'
+
+describe('Delete Product by id', () => {
+  beforeAll(() => {
+    inMemoryProductsRepository = new InMemoryProductsRepository()
+
+    sut = new DeleteProductService(inMemoryProductsRepository)
+
+    inMemoryProductsRepository.items.push({
+      id,
+      name: 'New Product',
+      description: 'New Product description',
+      price: '10',
+    })
+  })
+
+  it('should delete a product', async () => {
+    expect(inMemoryProductsRepository.items).toHaveLength(1)
+
+    const result = await sut.execute({ id })
+
+    expect(result.isRight()).toBeTruthy()
+    expect(inMemoryProductsRepository.items).toHaveLength(0)
+  })
+  it('shouldnt delete a product if id was wrong', async () => {
+    const result = await sut.execute({ id: 'error id' })
+
+    expect(result.isLeft()).toBeTruthy()
+  })
+})
