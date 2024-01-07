44bdc9d5709520c0d5b353bd50b5d19274a8e6f7
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-07 12:15:34 -0300

    feat: add create product spec

---
+++ src/services/product/create.spec.ts
@@ -0,0 +1,47 @@
+import { InMemoryProductsRepository } from '@/repositories/test/InMemoryProductsRepository'
+import { CreateProductService } from './create'
+
+let sut: CreateProductService
+let inMemoryProductsRepository: InMemoryProductsRepository
+
+describe('Create Product', () => {
+  beforeAll(() => {
+    inMemoryProductsRepository = new InMemoryProductsRepository()
+
+    sut = new CreateProductService(inMemoryProductsRepository)
+  })
+
+  it('should create a new product', async () => {
+    const newProduct = {
+      name: 'New Product',
+      description: 'New Product description',
+      price: '10',
+    }
+
+    const result = await sut.execute(newProduct)
+
+    expect(result.isRight()).toBeTruthy()
+    expect(inMemoryProductsRepository.items).toHaveLength(1)
+    expect(inMemoryProductsRepository.items[0]).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        name: 'New Product',
+        description: 'New Product description',
+        price: '10',
+      }),
+    )
+  })
+  it('shouldnt create a new product if the product already exist on database', async () => {
+    const newProduct = {
+      name: 'New Product',
+      description: 'New Product description',
+      price: '10',
+    }
+
+    await sut.execute(newProduct)
+    const result = await sut.execute(newProduct)
+
+    expect(result.isLeft()).toBeTruthy()
+    expect(inMemoryProductsRepository.items).toHaveLength(1)
+  })
+})
