fc7b1914084dc0535d1852252218b2742b43b5dc
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-08 19:18:28 -0300

    feat: add routes on app

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,7 +1,12 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 
 import { z } from 'zod'
 
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test', override: true })
+} else {
+  config()
+}
 export const envSchema = z.object({
   DATABASE_URL: z.string(),
   NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
--- src/http/controlers/products/index.ts
+++ src/http/controlers/products/index.ts
@@ -1,6 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { CreateProductController } from './Create'
+import { FindProductByIdController } from './FindUnique'
+import { FindAllProductsController } from './FindAll'
+import { DeleteProductController } from './Delete'
+import { EditProductController } from './Edit'
 
 export async function productsRoutes(app: FastifyInstance) {
   app.post('/product', CreateProductController)
+  app.get('/product', FindAllProductsController)
+  app.get('/product/:id', FindProductByIdController)
+  app.delete('/product/:id', DeleteProductController)
+  app.put('/product/:id', EditProductController)
 }
