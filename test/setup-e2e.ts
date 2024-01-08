8f9b160c51b65d46b2f1a7ff0c5cf1d85d7b78b1
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-08 10:40:08 -0300

    feat: add config to run test E2E

---
+++ test/setup-e2e.ts
@@ -0,0 +1,38 @@
+import { config } from 'dotenv'
+
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+import { envSchema } from '@/env'
+
+config({ path: '.env', override: true })
+config({ path: '.env.test', override: true })
+
+const env = envSchema.parse(process.env)
+
+const prisma = new PrismaClient()
+
+function generateUniqueDataBaseURL(schemaId: string) {
+  if (!env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is required')
+  }
+  const url = new URL(env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const schemaId = randomUUID()
+beforeAll(async () => {
+  const databaseURL = generateUniqueDataBaseURL(schemaId)
+
+  process.env.DATABASE_URL = databaseURL
+
+  execSync('pnpm prisma migrate deploy')
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+  await prisma.$disconnect()
+})
