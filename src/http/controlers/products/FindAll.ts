44b3fbb7e43ab8c12099fb8404481dde95c3af9e
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-08 19:17:42 -0300

    feat: add create find all products controller

---
+++ src/http/controlers/products/FindAll.ts
@@ -0,0 +1,22 @@
+import { PrismaProductsRepository } from '@/repositories/prisma/PrismaProductsRepository'
+import { prisma } from '@/repositories/prisma/connection'
+import { FindAllProductService } from '@/services/product/FindAll'
+
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function FindAllProductsController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const repository = new PrismaProductsRepository(prisma)
+
+  const service = new FindAllProductService(repository)
+
+  const result = await service.execute()
+
+  if (result.isLeft()) {
+    return reply.status(409).send(result.value)
+  }
+
+  reply.status(200).send({ products: result.value.products })
+}
