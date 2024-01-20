88612b0f4a2d9b825cf30b26dda0d16ebc1cb409
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-19 21:22:12 -0300

    feat: implement edit user service

---
+++ src/services/user/Edit.ts
@@ -0,0 +1,65 @@
+import { Either, left, right } from '@/erros/either'
+import { UserRepository } from '@/repositories/UserRepository'
+import { ResourceNotFoundError } from '../erros/ResourceNotFoundError'
+import { Comparer } from '@/cryptography/comparer'
+import { WrongCredentialsError } from '../erros/WrongCredentialsError'
+import { ResourceAlreadyExistError } from '../erros/ResourceAlreadyExistError'
+import { Hasher } from '@/cryptography/hasher'
+
+type EditUserRequest = {
+  id: string
+  name?: string
+  newEmail?: string
+  oldPassword: string
+  newPassword?: string
+}
+
+type EditUserResponse = Either<
+  ResourceNotFoundError | WrongCredentialsError | ResourceAlreadyExistError,
+  object
+>
+
+export class EditUserService {
+  constructor(
+    private userRepository: UserRepository,
+    private comparer: Comparer,
+    private hasher: Hasher,
+  ) {}
+
+  async execute({
+    id,
+    name,
+    oldPassword,
+    newEmail,
+    newPassword,
+  }: EditUserRequest): Promise<EditUserResponse> {
+    const user = await this.userRepository.findById(id)
+
+    if (!user) return left(new ResourceNotFoundError())
+
+    const userIsValid = await this.comparer.compare(oldPassword, user.password)
+
+    if (!userIsValid) return left(new WrongCredentialsError())
+
+    if (newEmail) {
+      const emailAlreadyExists = await this.userRepository.findByEmail(newEmail)
+
+      if (emailAlreadyExists) return left(new ResourceAlreadyExistError())
+    }
+
+    const email = newEmail || user.email
+
+    const password = newPassword
+      ? await this.hasher.hash(newPassword)
+      : user.password
+
+    await this.userRepository.update({
+      id,
+      name: name || user.name,
+      email,
+      password,
+    })
+
+    return right({})
+  }
+}
