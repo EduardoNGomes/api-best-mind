ebfaabe0e8892bb7b3aa922bad112053e18df4e9
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-22 11:54:07 -0300

    feat: implement authenticate service

---
+++ src/services/authenticate/Authenticate.ts
@@ -0,0 +1,40 @@
+import { Either, left, right } from '@/erros/either'
+import { AuthenticateRepository } from '@/repositories/AuthenticateRepository'
+import { WrongCredentialsError } from '../erros/WrongCredentialsError'
+import { Comparer } from '@/cryptography/comparer'
+import { Encrypter } from '@/cryptography/Encrypter'
+
+type AuthenticateUserRequest = {
+  email: string
+  password: string
+}
+
+type AuthenticateUserResponse = Either<
+  WrongCredentialsError,
+  { accessToken: { token: string } }
+>
+
+export class AuthenticateUserService {
+  constructor(
+    private authenticateRepository: AuthenticateRepository,
+    private comparer: Comparer,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUserRequest): Promise<AuthenticateUserResponse> {
+    const user = await this.authenticateRepository.findByEmail({ email })
+
+    if (!user) return left(new WrongCredentialsError())
+
+    const isUserValid = await this.comparer.compare(password, user.password)
+
+    if (!isUserValid) return left(new WrongCredentialsError())
+
+    const token = await this.encrypter.encrypt({ sub: user.id })
+
+    return right({ accessToken: { token } })
+  }
+}
