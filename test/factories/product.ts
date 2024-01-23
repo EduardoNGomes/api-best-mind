2ce77acc0690bfedf15bf58c69a4a47bfb356598
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-23 20:16:07 -0300

    feat: implement new method on factory to create product

--- test/factories/product.ts
+++ test/factories/product.ts
@@ -1,6 +1,7 @@
 import { ProductToCreateProps } from '@/repositories/ProductRepository'
 import { randomUUID } from 'crypto'
 import { faker } from '@faker-js/faker'
+import { Products } from '@prisma/client'
 
 class ProductFactory {
   createProductToUnitTest(data: Partial<ProductToCreateProps>) {
@@ -12,6 +13,21 @@ class ProductFactory {
       image: data.image ?? faker.word.words(1),
       createdAt: (data.createdAt as Date) ?? faker.date.anytime(),
       updatedAt: (data.updatedAt as Date) ?? faker.date.anytime(),
+    }
+    return product
+  }
+
+  async createProductToE2ETest(
+    data: Partial<ProductToCreateProps>,
+  ): Promise<Products> {
+    const product = {
+      id: data.id ?? randomUUID(),
+      name: data.name ?? faker.word.words(1),
+      description: data.description ?? faker.lorem.lines(1),
+      price: data.price ?? faker.number.int(),
+      image: data.image ?? faker.word.words(1),
+      createdAt: (data.createdAt as Date) ?? undefined,
+      updatedAt: (data.updatedAt as Date) ?? undefined,
     }
     return product
   }
