80420618af37b3ef214e9158288fb6f48d2e0c8d
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-23 19:25:41 -0300

    feat: implement create user E2E spec

---
+++ src/http/controllers/users/Create.e2e-spec.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import { app } from '@/server'
+
+describe('[E2E]/user ', async () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should create an state', async () => {
+    const response = await request(app.server).post('/user').send({
+      email: 'johnDoe@email.com',
+      password: '123456',
+      name: 'john Doe',
+    })
+
+    expect(response.status).toEqual(201)
+  })
+})
