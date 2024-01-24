c4a4f19650e9546c838fa6018586929213db574e
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-24 15:48:28 -0300

    feat:implement method to handle error from zod

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { join } from 'path'
 import staticFiles from '@fastify/static'
 import JWT from '@fastify/jwt'
 import cookie from '@fastify/cookie'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
@@ -21,4 +22,20 @@ app.register(Routes)
 
 app.register(staticFiles, {
   root: join(UPLOADS_FOLDER),
+})
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'internal server error.' })
 })
